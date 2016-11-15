cdc2e837158697ee7aa1105197852f28cf98ed1b
AoifeBrown <aoiferoisin@yahoo.co.uk>
2016-11-15 12:49:47 +0000

    Added the game objects

--- app/src/main/java/com/example/societyslam/societyslam/Game/MainActivity.java
+++ app/src/main/java/com/example/societyslam/societyslam/Game/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.societyslam.societyslam;
+package com.example.societyslam.societyslam.Game;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.societyslam.societyslam.R;
 
 
 public class MainActivity extends AppCompatActivity {
